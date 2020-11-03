2a329630e239f2a782a998e407d3cc4766a19d98
elton lazzarin <eltonlazzarin3@gmail.com>
2020-11-03 11:52:13 +0300

    update: [homepage] add Content layout in the homepage

--- pages/index.jsx
+++ pages/index.jsx
@@ -9,50 +9,53 @@ import { TestmonialSection } from "../components/global";
 import { Header, Footer } from "../components/global";
 import { useSelector } from "react-redux";
 import { Layout } from "antd";
+const { Content } = Layout;
 
 export default function Home(routerProps) {
   return (
     <Layout>
       <Header/>
-      <Main>
-        <Helmet>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <title>Mr Again | De beste telefoon reparateurs </title>
-          <meta
-            name="Keywords"
-            content="Telefoon reparatie, Telefoon reparateurs, Scherm vervangen, Batterij vervangen, water schade, MrAgain, Tablet reparatie, Tablet reparateurs, telefoonscherm vervangen, scherm telefoon kapot, telefoonscherm kapot, waterschade telefoon, telefoon in water laten vallen, iphone 6 batterij vervangen, nieuwe batterij iphone 7, iphone reparateur, telefoon in wc gevallen, scherm reparatie, iphone glas vervangen, kapot scherm, iphone glas vervangen, scherm iphone 6, nieuw scherm iphone 6, iphone 6 glas vervangen, telefoonscherm reparatie, scherm ipad vervangen"
-          />
-          <meta
-            name="description"
-            content="Telefoon kapot? Bij MrAgain vind je snel en gemakkelijk de beste telefoon reparateurs bij jou in de buurt."
-          />
-          <link
-            rel="canonical"
-            href={ FRONT_END_URL }
-          />
+      <Content>
+        <Main>
+          <Helmet>
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <title>Mr Again | De beste telefoon reparateurs </title>
+            <meta
+              name="Keywords"
+              content="Telefoon reparatie, Telefoon reparateurs, Scherm vervangen, Batterij vervangen, water schade, MrAgain, Tablet reparatie, Tablet reparateurs, telefoonscherm vervangen, scherm telefoon kapot, telefoonscherm kapot, waterschade telefoon, telefoon in water laten vallen, iphone 6 batterij vervangen, nieuwe batterij iphone 7, iphone reparateur, telefoon in wc gevallen, scherm reparatie, iphone glas vervangen, kapot scherm, iphone glas vervangen, scherm iphone 6, nieuw scherm iphone 6, iphone 6 glas vervangen, telefoonscherm reparatie, scherm ipad vervangen"
+            />
+            <meta
+              name="description"
+              content="Telefoon kapot? Bij MrAgain vind je snel en gemakkelijk de beste telefoon reparateurs bij jou in de buurt."
+            />
+            <link
+              rel="canonical"
+              href={ FRONT_END_URL }
+            />
 
-          {/**Below mentioned meta tags are og tags that are used when website is through any socaial media.*/}
-          <meta property="og:type" content="website" />
-          <meta name="og_title" property="og:title" content="Mr Again - Home" />
+            {/**Below mentioned meta tags are og tags that are used when website is through any socaial media.*/}
+            <meta property="og:type" content="website" />
+            <meta name="og_title" property="og:title" content="Mr Again - Home" />
 
-          <meta
-            property="og:description"
-            content="Vind de beste reparateur bij jou in de buurt"
-          />
-          <meta
-            name="og:url"
-            content={ FRONT_END_URL }
-          />
-          <meta property="og:image" content="" />
-          <meta name="og_site_name" property="og:site_name" content="Mr Again" />
+            <meta
+              property="og:description"
+              content="Vind de beste reparateur bij jou in de buurt"
+            />
+            <meta
+              name="og:url"
+              content={ FRONT_END_URL }
+            />
+            <meta property="og:image" content="" />
+            <meta name="og_site_name" property="og:site_name" content="Mr Again" />
 
-          <meta name="theme-color" content="#ffffff" />
-        </Helmet>
-        <FindBannerSection />
-        <AdvantageSection />
-        <NewestShopsSection />
-        <TestmonialSection />
-      </Main>
+            <meta name="theme-color" content="#ffffff" />
+          </Helmet>
+          <FindBannerSection />
+          <AdvantageSection />
+          <NewestShopsSection />
+          <TestmonialSection />
+        </Main>
+      </Content>
       <Footer/>
     </Layout>
   )
