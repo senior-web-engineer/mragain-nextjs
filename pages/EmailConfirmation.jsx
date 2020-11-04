a34ac89dfc0f9b9087d9bccd2907e68f5bd12023
elton lazzarin <eltonlazzarin3@gmail.com>
2020-11-05 00:44:42 +0300

    add: [EmailConfirmation page] add EmailConfirmation page

---
+++ pages/EmailConfirmation.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { BannerSection } from "@/components/home/FindBannerSection/FindBannerSection.style";
+import { Layout } from "@/components/global";
+
+const EmailConfirmation = () => {
+  return ( 
+    <Layout>
+      <BannerSection />
+    </Layout>
+  )
+}
+export default EmailConfirmation;
