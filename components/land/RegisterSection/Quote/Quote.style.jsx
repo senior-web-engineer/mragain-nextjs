ad683738d4736df2c878b43777f914a1f750e245
butterfly-coder <butterfly17123@protonmail.com>
2021-04-13 14:45:14 -0300

    update: [register-page] update the register page for the mobile version

--- components/land/RegisterSection/Quote/Quote.style.jsx
+++ components/land/RegisterSection/Quote/Quote.style.jsx
@@ -7,4 +7,8 @@ export const QuoteArea = styled.div`
   background-color: #f1fefa;
   border-radius: 20px;
   padding: 310px 100px 75px 100px;
+  @media (max-width:768px) {
+    border-radius: 0px;
+    padding: 30px;
+  }
 `;
--- components/land/RegisterSection/RegisterSection.style.jsx
+++ components/land/RegisterSection/RegisterSection.style.jsx
@@ -4,4 +4,7 @@ export const RegisterSectionArea = styled.div`
   color: #000;
   width: 100%;
   padding: 30px;
+  @media (max-width:768px) {
+    padding: 0px;
+  }
 `;
