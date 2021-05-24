68f102abf3b12efba8a214e362753c3c6337db96
Pim_Meurs <pim.meurs@gmail.com>
2021-05-24 12:03:31 +0200

    translations

--- pages/search-results.jsx
+++ pages/search-results.jsx
@@ -973,7 +973,7 @@ const RefineModalWrap = styled.div`
   }
 `;
 
-function ClearFilters({ label = "Clear", alwaysShow = false }) {
+function ClearFilters({ label = "Reset", alwaysShow = false }) {
   const { state, actions } = useFormContext();
 
   const hasDiff = !isEqual(state.initialValues, state.values);
@@ -1043,7 +1043,7 @@ function ResultCount() {
     return null;
   }
 
-  return ` (${context.items.length} )`;
+  return ` (${context.items.length})`;
 }
 
 export default function SearchResults() {
