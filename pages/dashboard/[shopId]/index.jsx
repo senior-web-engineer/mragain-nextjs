9803560af12e053f1e4d2d1d78635dea4e97f1c7
Pim_Meurs <pim.meurs@gmail.com>
2021-05-30 11:03:26 +0200

    test

--- pages/dashboard/[shopId]/index.jsx
+++ pages/dashboard/[shopId]/index.jsx
@@ -273,7 +273,7 @@ const ShopDashboard = (routerProps) => {
     setPhone(el.device.id);
     setBrand(el.brand.id);
     setModel(el.model.id);
-    setReparation(el.reparation.id);
+    setReparation(el.reparation.reparation_name);
     setShowModal(true);
     setSelectRepId(el.id);
     setGuarantee(el.guarnantee);
