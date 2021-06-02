e584a368a3674b57bc8976f6f837eafe1ecb7f13
Pim_Meurs <pim.meurs@gmail.com>
2021-06-02 08:31:01 +0200

    text

--- components/home/FindSection/index.jsx
+++ components/home/FindSection/index.jsx
@@ -197,7 +197,7 @@ export default function FindSection() {
   return (
     <FindWrap>
       <SearchWrap>
-        <h1>Vind snel de beste reparateurs</h1>
+        <h1>Vind snel een reparateur</h1>
         <Form module={searchForm}>
           <SearchBar>
             <div>
