50f9b8c60c7ec2fc8bf2b777e0b9def9a06fd398
Pim_Meurs <pim.meurs@gmail.com>
2021-07-14 22:18:20 +0200

    default sort on distance

--- components/home/FindSection/index.jsx
+++ components/home/FindSection/index.jsx
@@ -153,7 +153,7 @@ function SearchButton() {
 
   return (
     <Link
-      href={`/search-results?zip=${zip}&device=${device}&long=${currentLocation.long}&lat=${currentLocation.lat}&distance=30`}
+      href={`/search-results?zip=${zip}&device=${device}&long=${currentLocation.long}&lat=${currentLocation.lat}&distance=30&sort=8`}
     >
       <Button aria-label="Zoek" as="a">
         <span>Zoek</span>
