41a6802e26703d714e9258c1423c4ae8477090fd
Pim_Meurs <pim.meurs@gmail.com>
2021-07-17 15:21:10 +0200

    added cities to footer

--- components/global/footer/Footer.jsx
+++ components/global/footer/Footer.jsx
@@ -332,6 +332,31 @@ const FooterView = (routerProps) => {
 	             Den Haag
 	             </Link>
 	           </li>
+	           <li>
+	             <Link prefetch={false} href="/search-results?zip=Utrecht,%20Nederland&device=0&long=5.1214201&lat=52.09073739999999&distance=30&sort=8">
+	             Utrecht
+	             </Link>
+	           </li>
+	           <li>
+	             <Link prefetch={false} href="/search-results?zip=Groningen,%20Nederland&device=0&long=6.5665017&lat=53.2193835&distance=30&sort=8">
+	             Groningen
+	           </Link>
+	           </li>
+	           <li>
+	             <Link prefetch={false} href="/search-results?zip=Den%20Bosch,%20Nederland&device=0&long=5.3036748&lat=51.6978162&distance=30&sort=8">
+	             Den Bosch
+	             </Link>
+	           </li>
+	           <li>
+	             <Link prefetch={false} href="/search-results?zip=Tilburg,%20Nederland&device=0&long=5.0919143&lat=51.560596&distance=30&sort=8">
+	             Tilburg
+	             </Link>
+	           </li>
+	           <li>
+	             <Link prefetch={false} href="/search-results?zip=Maastricht,%20Nederland&device=0&long=5.6909725&lat=50.8513682&distance=30&sort=8">
+	             Maastricht
+	             </Link>
+	           </li>
 	         </ul>
                </FooterSitemap>
           </FooterLinkArea>{" "}
