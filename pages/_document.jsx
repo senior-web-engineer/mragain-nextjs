0a9239e9e2d56b74018bd2ef64f70bd70739a687
Pim_Meurs <pim.meurs@gmail.com>
2021-09-30 22:26:01 +0200

    GA tags

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -83,6 +83,14 @@ export default class MyDocument extends Document {
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
           ></script>
+	  <!-- Google Tag Manager -->
+	  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+		    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+			    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+			    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	    })(window,document,'script','dataLayer','GTM-PVDCX7L');
+	  </script>
+	  <!-- End Google Tag Manager -->
           <noscript>
             <img
               height="1"
@@ -101,6 +109,11 @@ export default class MyDocument extends Document {
           ) : null}                 
         </HeadWithoutPreload>
         <body>
+	  <!-- Google Tag Manager (noscript) -->
+	  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PVDCX7L"
+	    height="0" width="0" style="display:none;visibility:hidden"></iframe>
+	  </noscript>
+	  <!-- End Google Tag Manager (noscript) -->
           <Main />
           <NextScript />
           {isProduction ? (
