2aa3ab0ac09871716381142622e735ce6b151773
Pim_Meurs <pim.meurs@gmail.com>
2021-10-01 19:14:14 +0200

    test

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -16,6 +16,15 @@ const hotJarScript = `
 `;
 
 const gaScript = `
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', '${GA_TRACKING_ID}', {
+  page_path: window.location.pathname,
+});
+`;
+
+const gaHeadScript = `
   (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
   new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
   j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
