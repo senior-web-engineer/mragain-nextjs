ba98031786dbd78c214935a5360a727c2d311b2b
Pim_Meurs <pim.meurs@gmail.com>
2021-10-03 21:03:25 +0200

    revert

--- components/appointment/LocationSelector.jsx
+++ components/appointment/LocationSelector.jsx
@@ -10,7 +10,7 @@ export const LOCATION_OPTIONS = [
     description: "Kom bij ons langs voor je reparatie",
     icon: faStore,
     value: "in-store",
-    apiValue: "1",
+    apiValue: "2",
   },
   {
     label: "Reparatie op locatie",
@@ -18,7 +18,7 @@ export const LOCATION_OPTIONS = [
     description: "Wij komen naar jou!",
     icon: faHome,
     value: "home",
-    apiValue: "2"
+    apiValue: "1"
   },
   // {
   //   label: "Opsturen",
