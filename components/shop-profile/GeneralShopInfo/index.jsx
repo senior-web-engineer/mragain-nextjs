3b48ebd318595464b4d40ae8aa99886ae91eaa77
Hasan Javed <hasanjaved065@gmail.com>
2021-12-01 00:33:35 +0500

    Fix alignment issue

--- components/shop-profile/GeneralShopInfo/index.jsx
+++ components/shop-profile/GeneralShopInfo/index.jsx
@@ -1,8 +1,8 @@
-import { MaxConstraints } from "@/components/styled/layout";
 import { Col, Row } from "antd";
 import React from "react";
 import styled from "styled-components";
 
+import { MaxConstraints } from "@/components/styled/layout";
 
 const MainWrap = styled.div`
   background-color: #f3f3f3;
@@ -20,7 +20,7 @@ const Header = styled.h3`
   font-size: 24px;
   line-height: 14px;
   color: #404040;
-  margin-bottom: 24px;
+  padding-bottom: 24px;
 `;
 
 const Text = styled.p`
@@ -32,21 +32,30 @@ const Text = styled.p`
   color: #404040;
 `;
 
+const StyledCol = styled(Col)`
+  margin: 24px 0;
+`;
+
 export default function GeneralShopInfo({ shopGeneralInfo }) {
-    return (
-        <MainWrap>
-            <MaxConstraints>
-                <RowWrap>
-                    <Row>
-                        {shopGeneralInfo?.content?.map((info) => (
-                            <Col span={12}>
-                                <Header>{info?.header}</Header>
-                                <Text>{info?.content}</Text>
-                            </Col>
-                        ))}
-                    </Row>
-                </RowWrap>
-            </MaxConstraints>
-        </MainWrap>
-    );
+  return (
+    <MainWrap>
+      <MaxConstraints>
+        <RowWrap>
+          <Row
+            gutter={{
+              sm: 72,
+              xs: 12,
+            }}
+          >
+            {shopGeneralInfo?.content?.map((info) => (
+              <StyledCol xs={24} sm={12} md={12}>
+                <Header>{info?.header}</Header>
+                <Text>{info?.content}</Text>
+              </StyledCol>
+            ))}
+          </Row>
+        </RowWrap>
+      </MaxConstraints>
+    </MainWrap>
+  );
 }
