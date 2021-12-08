11de0ea9bbf6718eddb8cde5d4938381fd831337
Pim_Meurs <pim.meurs@gmail.com>
2021-12-08 21:13:08 +0100

    title city pages

--- pages/[city].jsx
+++ pages/[city].jsx
@@ -41,14 +41,8 @@ export default function City({ cityInfo }) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>
-          De beste telefoon reparateur bij jou in de buurt | Mr Again{" "}
+	Telefoon Reparatie {cityContent?.title} | Mr Again{" "}
         </title>
-
-        <meta
-          name="Keywords"
-          content="Telefoon reparatie, Telefoon reparateurs, Scherm vervangen, Batterij vervangen, water schade, MrAgain, Tablet reparatie, Tablet reparateurs, telefoonscherm vervangen, scherm telefoon kapot, telefoonscherm kapot, waterschade telefoon, telefoon in water laten vallen, iphone 6 batterij vervangen, nieuwe batterij iphone 7, iphone reparateur, telefoon in wc gevallen, scherm reparatie, iphone glas vervangen, kapot scherm, iphone glas vervangen, scherm iphone 6, nieuw scherm iphone 6, iphone 6 glas vervangen, telefoonscherm reparatie, scherm ipad vervangen"
-        />
-        <meta
           name="description"
           content="Telefoon kapot? Bij MrAgain vind je snel en gemakkelijk de beste telefoon reparateurs bij jou in de buurt."
         />
