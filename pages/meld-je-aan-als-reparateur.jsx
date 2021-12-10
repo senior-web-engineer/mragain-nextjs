f70526bbda85cf8367b1650884b64f0304b28880
Pim_Meurs <pim.meurs@gmail.com>
2021-12-10 19:02:17 +0100

    fix

--- pages/meld-je-aan-als-reparateur.jsx
+++ pages/meld-je-aan-als-reparateur.jsx
@@ -6,6 +6,7 @@ import DefaultLayout from "@/components/layouts/Homepage";
 import { RegisterSection } from '../components/land'
 import { Main } from '@/styled-components/meld-je-aan-als-reparateur.style.jsx'
 import './meld-je-aan-als-reparateur.style.less';
+import Head from "next/head";
 
 const Land = () => {
   useEffect(() => {
