042280a914e2a475c51c4ab12f237c3059c9f704
Pim_Meurs <pim.meurs@gmail.com>
2021-12-10 19:11:27 +0100

    fix

--- pages/meld-je-aan-als-reparateur.jsx
+++ pages/meld-je-aan-als-reparateur.jsx
@@ -7,6 +7,7 @@ import { RegisterSection } from '../components/land'
 import { Main } from '@/styled-components/meld-je-aan-als-reparateur.style.jsx'
 import './meld-je-aan-als-reparateur.style.less';
 import Head from "next/head";
+import { FRONT_END_URL } from "@constants";
 
 const Land = () => {
   useEffect(() => {
