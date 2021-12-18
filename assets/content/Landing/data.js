ea16315d11f9b6e3d1cf7f7c43b19d767d775dcd
Pim_Meurs <pim.meurs@gmail.com>
2021-12-18 14:00:27 +0100

    styling

--- assets/content/Landing/data.js
+++ assets/content/Landing/data.js
@@ -5,15 +5,15 @@ const posts = [
     description: [
       {
         content:
-	"Batterij vervangen, waterschade en scherm vervangen zijn de top drie telefoon reparaties die worden uitgevoerd bij telefoon reparateurs. De telefoon batterij vervangen verlengt de levensduur van je telefoon en voorkomt dat deze constant aan de oplader moet."      
+	"<p>Batterij vervangen, waterschade en scherm vervangen zijn de top drie telefoon reparaties die worden uitgevoerd bij telefoon reparateurs. De telefoon batterij vervangen verlengt de levensduur van je telefoon en voorkomt dat deze constant aan de oplader moet.<br></p>"      
       },
       {
         content:
-	"Is je telefoon in het water gevallen? Ga dan snel naar een telefoonwinkel voor een waterschade behandeling. Als je geluk hebt valt de schade mee. Leg de telefoon niet in een bak met rijst zoals op internet gesuggereerd wordt. Dit helpt oppervlakkig maar zorgt er niet voor dat het vocht bij kritische componenten wordt weggehaald. Hierdoor loop je het risico op meer schade."
+	"<p>Is je telefoon in het water gevallen? Ga dan snel naar een telefoonwinkel voor een waterschade behandeling. Als je geluk hebt valt de schade mee. Leg de telefoon niet in een bak met rijst zoals op internet gesuggereerd wordt. Dit helpt oppervlakkig maar zorgt er niet voor dat het vocht bij kritische componenten wordt weggehaald. Hierdoor loop je het risico op meer schade.<br></p>"
       },
       {
         content:
-	"Functioneert de smartphone die je hebt gekocht niet meer naar behoren? Via MrAgain zie je snel wie jouw toestel kan repareren en bijvoorbeeld de batterij van je iPhone kan vervangen. Datzelfde geldt voor je telefoon scherm vervangen en telefoon maken in algemene zin. "
+	"<p>Functioneert de smartphone die je hebt gekocht niet meer naar behoren? Via MrAgain zie je snel wie jouw toestel kan repareren en bijvoorbeeld de batterij van je iPhone kan vervangen. Datzelfde geldt voor je telefoon scherm vervangen en telefoon maken in algemene zin.<br></p> "
       },
     ],
   },
@@ -23,27 +23,27 @@ const posts = [
     description: [
       {
         content:
-	"Heb je een barst in je iPhone, wil je de iPhone batterij vervangen of is hij in een plas water gevallen? Wat de oorzaak ook is, natuurlijk wil je dat je iPhone het zo snel mogelijk weer doet."
+	"<p>Heb je een barst in je iPhone, wil je de iPhone batterij vervangen of is hij in een plas water gevallen? Wat de oorzaak ook is, natuurlijk wil je dat je iPhone het zo snel mogelijk weer doet.<br></p>"
       },
       {
         content:
-	"Voor iPhone reparaties is het verstandig om contact op te nemen met een telefoon reparateur. Deze heeft de onderdelen en expertise om je telefoon te repareren. Vaak zijn reparateurs erkende Apple serviceproviders en werken ze met originele Apple onderdelen. Hierdoor krijgt je telefoon dezelfde onderdelen als een nieuwe iPhone, zoals bijvoorbeeld een originele batterij." 
+	"<p>Voor iPhone reparaties is het verstandig om contact op te nemen met een telefoon reparateur. Deze heeft de onderdelen en expertise om je telefoon te repareren. Vaak zijn reparateurs erkende Apple serviceproviders en werken ze met originele Apple onderdelen. Hierdoor krijgt je telefoon dezelfde onderdelen als een nieuwe iPhone, zoals bijvoorbeeld een originele batterij.<br></p>" 
       },
       {
         content:
-	"In de meeste gevallen kunnen reparaties snel uitgevoerd worden. Zo kan het vervangen van de batterij meestal binnen 30 minuten. Je hebt jouw iPhone met een nieuwe batterij dus vaak zo weer terug. Duurt de reparatie toch langer? Dan is het vaak mogelijk om een leen toestel mee te krijgen voor het tijdelijk vervangen van de iPhone." 
+	"<p>In de meeste gevallen kunnen reparaties snel uitgevoerd worden. Zo kan het vervangen van de batterij meestal binnen 30 minuten. Je hebt jouw iPhone met een nieuwe batterij dus vaak zo weer terug. Duurt de reparatie toch langer? Dan is het vaak mogelijk om een leen toestel mee te krijgen voor het tijdelijk vervangen van de iPhone.<br></p>" 
       },
       {
         content:
-	"Voor minder voorkomende defecten is het verstandig van tevoren te controleren wie deze reparaties uitvoert. Denk hierbij aan het vervangen van je backcover of camera. Niet elke reparateur biedt deze aan."
+	"<p>Voor minder voorkomende defecten is het verstandig van tevoren te controleren wie deze reparaties uitvoert. Denk hierbij aan het vervangen van je backcover of camera. Niet elke reparateur biedt deze aan.<br></p>"
       },
       {
         content:
-	"Is je telefoon niet kapot, maar wil je data overzetten of je telefoon van nieuwe software voorzien? Ook dan ben je bij de meeste telefoon reparateurs aan het juiste adres. Vaak kun je terecht voor de reparaties vanaf iPhone 5 en iPhone 6" 
+	"<p>Is je telefoon niet kapot, maar wil je data overzetten of je telefoon van nieuwe software voorzien? Ook dan ben je bij de meeste telefoon reparateurs aan het juiste adres. Vaak kun je terecht voor de reparaties vanaf iPhone 5 en iPhone 6<br></p>" 
       },
       {
         content:
-	"Heb je nog een iphone 4? Check dan via MrAgain wie ook die nog repareert. Ook zie je snel wat het kost en hoeveel maanden garantie je krijgt. Je kunt dan ook direct een afspraak maken zodat je iPhone het zo snel mogelijk weer doet."
+	"<p>Heb je nog een iphone 4? Check dan via MrAgain wie ook die nog repareert. Ook zie je snel wat het kost en hoeveel maanden garantie je krijgt. Je kunt dan ook direct een afspraak maken zodat je iPhone het zo snel mogelijk weer doet.<br></p>"
       },
     ],
   },
@@ -53,15 +53,15 @@ const posts = [
     description: [
       {
         content:
-	"Een kapotte telefoon maar geen tijd om naar de telefoonwinkel te gaan? Maak je geen zorgen: veel telefoon reparateurs bieden tegenwoordig reparatie op locatie aan. Een extra service waarbij de reparateur naar jou toekomt en je dus thuis kan blijven, of kan blijven werken."
+	"<p>Een kapotte telefoon maar geen tijd om naar de telefoonwinkel te gaan? Maak je geen zorgen: veel telefoon reparateurs bieden tegenwoordig reparatie op locatie aan. Een extra service waarbij de reparateur naar jou toekomt en je dus thuis kan blijven, of kan blijven werken.<br></p>"
       },
       {
         content:
-	"Gelukkig bieden de meeste reparateurs tegenwoordig een haal en breng service, of reparatie op locatie aan. Zo kun jij door met werken terwijl je telefoon bij je op de stoep gemaakt wordt."
+	"<p>Gelukkig bieden de meeste reparateurs tegenwoordig een haal en breng service, of reparatie op locatie aan. Zo kun jij door met werken terwijl je telefoon bij je op de stoep gemaakt wordt.<br></p>"
       },
       {
         content:
-	"Wil je snel weten welke reparateurs ook bij jou op locatie repareren? Gebruik dan het zoekfilter op onze website en en zie direct welke reparateurs ook bij jou komen. Ook zie je direct wat ze repareren en hoe duur de reparatie is. Natuurlijk kun je hiervoor ook makkelijk een afspraak maken via onze website."
+	"<p>Wil je snel weten welke reparateurs ook bij jou op locatie repareren? Gebruik dan het zoekfilter op onze website en en zie direct welke reparateurs ook bij jou komen. Ook zie je direct wat ze repareren en hoe duur de reparatie is. Natuurlijk kun je hiervoor ook makkelijk een afspraak maken via onze website.<br></p>"
       },
     ],
   },
@@ -71,11 +71,11 @@ const posts = [
     description: [
       {
         content:
-	"Als je een gebarsten scherm hebt doe je er verstandig aan deze snel te laten vervangen. Hierdoor voorkom je verdere schade, bijvoorbeeld doordat er vuil je telefoon in komt, en dat je je bezeert. Voor telefoon scherm reparatie is het belangrijk dat je oplet met de kwaliteit van het scherm dat je koopt. In de regel geldt dat hoe duurder het scherm is waarmee je het oude scherm vervangt,  hoe beter de kwaliteit is."
+	"<p>Als je een gebarsten scherm hebt doe je er verstandig aan deze snel te laten vervangen. Hierdoor voorkom je verdere schade, bijvoorbeeld doordat er vuil je telefoon in komt, en dat je je bezeert. Voor telefoon scherm reparatie is het belangrijk dat je oplet met de kwaliteit van het scherm dat je koopt. In de regel geldt dat hoe duurder het scherm is waarmee je het oude scherm vervangt,  hoe beter de kwaliteit is.<br></p>"
       },
       {
         content:
-	"Veel copy schermen zijn echter van zeer goede kwaliteit tegenwoordig, het is dus niet noodzakelijk dat je een origineel scherm laat plaatsen. Het vervangen van je telefoon scherm is een reparatie die normaal snel uitgevoerd kan worden (binnen 30 minuten). Dit geldt ook voor iPhone scherm reparaties."
+	"<p>Veel copy schermen zijn echter van zeer goede kwaliteit tegenwoordig, het is dus niet noodzakelijk dat je een origineel scherm laat plaatsen. Het vervangen van je telefoon scherm is een reparatie die normaal snel uitgevoerd kan worden (binnen 30 minuten). Dit geldt ook voor iPhone scherm reparaties.<br></p>"
       },
     ],
   },
@@ -89,11 +89,11 @@ const posts = [
       },
       {
 	content:
-	"Dus, je zoekt op iPhone scherm vervangen, maar welk scherm heb je nodig? Afhankelijk van je budget en de kwaliteit die je zoekt heb je meerdere opties. Bijvoorbeeld  een origineel scherm, een high-copy scherm of andere copy schermen."
+	"<p>Dus, je zoekt op iPhone scherm vervangen, maar welk scherm heb je nodig? Afhankelijk van je budget en de kwaliteit die je zoekt heb je meerdere opties. Bijvoorbeeld  een origineel scherm, een high-copy scherm of andere copy schermen.<br></p>"
       },
       {
         content:
-	"Natuurlijk geldt vaak dat hoe goedkoper het scherm is, hoe minder de kwaliteit van het scherm voor je smartphone reparatie. Maak daarbij dus een goede afweging en vraag advies aan de telefoon reparateur indien je twijfelt. Daarna snel erop laten zetten zodat je scherm weer perfect is."
+	"<p>Natuurlijk geldt vaak dat hoe goedkoper het scherm is, hoe minder de kwaliteit van het scherm voor je smartphone reparatie. Maak daarbij dus een goede afweging en vraag advies aan de telefoon reparateur indien je twijfelt. Daarna snel erop laten zetten zodat je scherm weer perfect is.<br></p>"
       },
     ],
   },	
@@ -103,15 +103,15 @@ const posts = [
     description: [
       {
 	content:
-	"Als je iPad of tablet kapot is kun je vaak ook bij telefoon reparateurs terecht. Voor de bekendere merken zoals Apple, Samsung en Huawei hebben ze vaak originele onderdelen op voorraad. Bijvoorbeeld schermen en accu’s waardoor je snel geholpen kunt worden."
+	"<p>Als je iPad of tablet kapot is kun je vaak ook bij telefoon reparateurs terecht. Voor de bekendere merken zoals Apple, Samsung en Huawei hebben ze vaak originele onderdelen op voorraad. Bijvoorbeeld schermen en accu’s waardoor je snel geholpen kunt worden.<br></p>"
       },
       {
 	content:
-	"Minder voorkomende reparaties zoals die aan het moederbord worden niet door elke reparateur uitgevoerd. In dit geval is het slim om vooraf te controleren wie deze reparatie wel kan uitvoeren."
+	"<p>Minder voorkomende reparaties zoals die aan het moederbord worden niet door elke reparateur uitgevoerd. In dit geval is het slim om vooraf te controleren wie deze reparatie wel kan uitvoeren.<br></p>"
       },
       {
         content:
-	"Datzelfde geldt voor de reparatie van specifieke modellen, zoals de oudere iPad versies, welke niet meer door iedereen worden gerepareerd."
+	"<p>Datzelfde geldt voor de reparatie van specifieke modellen, zoals de oudere iPad versies, welke niet meer door iedereen worden gerepareerd.<br></p>"
       },
     ],
   },
@@ -121,7 +121,7 @@ const posts = [
     description: [
       {
 	content:
-	"<p>Ook voor laptop reparatie kun je via onze website de beste reparateurs vinden. Dat geldt ook voor Macbook reparatie en alles wat met laptops te maken heeft. De meest voorkomende laptop reparaties zijn het vervangen van het scherm, vergroten van het geheugen met extra RAM en natuurlijk het vervangen van accu's.</p>"   
+	"<p>Ook voor laptop reparatie kun je via onze website de beste reparateurs vinden. Dat geldt ook voor Macbook reparatie en alles wat met laptops te maken heeft. De meest voorkomende laptop reparaties zijn het vervangen van het scherm, vergroten van het geheugen met extra RAM en natuurlijk het vervangen van accu's.<br></p>"   
       },
     ],
   },
@@ -131,11 +131,11 @@ const posts = [
     description: [
       {
 	content:
-	"<p>Via <a href=https://www.mragain.nl>MrAgain </a> vind je reparateurs door het hele land. Of je nu in de buurt van Maastricht, of Groningen op zoek bent, het maakt niet uit. Overal is er een reparateur die je kan helpen. Klik hier snel door naar een overzicht van reparateurs in de grootste steden.</p>"
+	"<p>Via <a href=https://www.mragain.nl>MrAgain </a> vind je reparateurs door het hele land. Of je nu in de buurt van Maastricht, of Groningen op zoek bent, het maakt niet uit. Overal is er een reparateur die je kan helpen. Klik hier snel door naar een overzicht van reparateurs in de grootste steden.<br></p>"
       },	      
       {
 	content:
-	"<p><ul><li><a href=https://www.mragain.nl/enschede>Telefoon reparatie Enschede</a></li> <li><a href=https://www.mragain.nl/almere>Telefoon reparatie Almere</a></li> <li><a href=https://www.mragain.nl/utrecht>Telefoon reparatie Utrecht</a></li><li><a href=https://www.mragain.nl/den-haag>Telefoon reparatie Den Haag</a></li><li><a href=https://www.mragain.nl/amsterdam>Telefoon reparatie Amsterdam</a></li><li><a href=https://www.mragain.nl/rotterdam>Telefoon reparatie Rotterdam</a></li><li><a href=https://www.mragain.nl/apeldoorn>Telefoon reparatie Apeldoorn</a>>/li><li><a href=https://www.mragain.nl/groningen>Telefoon reparatie Groningen</a></li><li><a href=https://www.mragain.nl/zoetermeer>Telefoon reparatie Zoetermeer</a></li><li><a href=https://www.mragain.nl/nijmegen>Telefoon reparatie Nijmegen</a></li><li><a href=https://www.mragain.nl/zwolle>Telefoon reparatie Zwolle</a></li><li><a href=https://www.mragain.nl/ede>Telefoon reparatie Ede</a></li></ul></p>"     
+	"<p><ul><li><a href=https://www.mragain.nl/enschede>Telefoon reparatie Enschede</a></li> <li><a href=https://www.mragain.nl/almere>Telefoon reparatie Almere</a></li> <li><a href=https://www.mragain.nl/utrecht>Telefoon reparatie Utrecht</a></li><li><a href=https://www.mragain.nl/den-haag>Telefoon reparatie Den Haag</a></li><li><a href=https://www.mragain.nl/amsterdam>Telefoon reparatie Amsterdam</a></li><li><a href=https://www.mragain.nl/rotterdam>Telefoon reparatie Rotterdam</a></li><li><a href=https://www.mragain.nl/apeldoorn>Telefoon reparatie Apeldoorn</a></li><li><a href=https://www.mragain.nl/groningen>Telefoon reparatie Groningen</a></li><li><a href=https://www.mragain.nl/zoetermeer>Telefoon reparatie Zoetermeer</a></li><li><a href=https://www.mragain.nl/nijmegen>Telefoon reparatie Nijmegen</a></li><li><a href=https://www.mragain.nl/zwolle>Telefoon reparatie Zwolle</a></li><li><a href=https://www.mragain.nl/ede>Telefoon reparatie Ede</a></li></ul></p>"     
       },
     ],
   },
