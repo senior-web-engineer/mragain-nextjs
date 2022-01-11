ee285c277895417a10cd60f48f5d1e2ab6fc177e
Pim_Meurs <pim.meurs@gmail.com>
2022-01-11 19:41:10 +0100

    seo

--- pages/devices/[device]/index.jsx
+++ pages/devices/[device]/index.jsx
@@ -6,12 +6,30 @@ import DefaultLayout from "@/components/layouts/Homepage";
 import { MaxConstraints } from "@/components/styled/layout";
 import { API_PATH } from "@/constants";
 import api from "@/utils/api";
+import Head from "next/head";
+import { FRONT_END_URL } from "@/constants.js";
 
 export default function Devices({ models, deviceName }) {
   const [searchTerm, updateSearchTerm] = useState("");
 
+let title = `${deviceName} reparatie - MrAgain.nl`;
+let description = `Op zoek naar ${deviceName} reparatie? Via MrAgain zie je direct wie je device kan maken!`;
+let url = `${FRONT_END_URL}/devices/${deviceName}`;
+
   return (
     <DefaultLayout>
+     <Head>
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<title>{title}</title>
+	<meta name="description" content={description}/>
+	<link rel="canonical" href={url} />
+	<meta property="og:type" content="website" />
+	<meta name="og_title" property="og:title" content={title}/>
+	<meta property="og:description" content={description}/>
+	<meta name="og:url" property="og:url" content={url} />
+	<meta name="og_site_name" property="og:site_name" content="Mr Again"
+	<meta name="theme-color" content="#ffffff" />
+    </Head>
       <MaxConstraints>
         <DeviceFinder
           models={models}
