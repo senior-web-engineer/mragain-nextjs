0f3f4f7e7b9cd930126ad0d70c3196baeac094e8
Pim_Meurs <pim.meurs@gmail.com>
2022-01-11 18:02:12 +0100

    seo fixes

--- pages/devices/index.jsx
+++ pages/devices/index.jsx
@@ -9,6 +9,18 @@ import api from "@/utils/api";
 export default function Devices({ models }) {
   return (
     <DefaultLayout>
+     <Head>
+	<title>Alle apparaten voor elektronica reparatie | MrAgain </title>
+	<meta name="description" content="Op zoek naar reparatie voor je telefoon, tablet of laptop? Vind snel je model en reparateurs die je kunnen helpen"/>
+	<link rel="canonical" href={FRONT_END_URL}/devices />
+	{/**Below mentioned meta tags are og tags that are used when website is through any socaial media.*/}
+	<meta property="og:type" content="website" />
+	<meta name="og_title" property="og:title" content="Alle apparaten voor elektronica reparatie | MrAgain" />
+	<meta property="og:description" content="Op zoek naar reparatie voor je telefoon, tablet of laptop? Vind snel je model en reparateurs die je kunnen helpen" />
+	<meta name="og:url" content={FRONT_END_URL}/devices />
+	<meta name="og_site_name" property="og:site_name" content="Mr Again" />
+	<meta name="theme-color" content="#ffffff" />
+     </Head>
       <MaxConstraints>
         <DeviceFinder models={models} />
       </MaxConstraints>
