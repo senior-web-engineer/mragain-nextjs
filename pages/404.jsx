b283054fb021550dbb6057691fd2476d0da85f9e
maksym simutin <simutin1801@gmail.com>
2022-01-24 17:20:59 +0200

    integrated the style file

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,3 +1,11 @@
+import Link from "next/link";
+import React from "react";
+import { connect } from "react-redux";
+
+import ArrowLeft from "@/assets/icons/arrow-left.svg";
+import Error404 from "@/assets/icons/error404.svg";
+import Error404_mobile from "@/assets/icons/error404_mobile.svg";
+import DefaultLayout from "@/components/layouts/Homepage";
 import styled from "styled-components";
 
 export const MainContainer = styled.div`
@@ -152,3 +160,49 @@ export const Error404PageLinks = styled.div`
     color: #059b68;
   }
 `;
+
+const Error404View = () => {
+  return (
+    <DefaultLayout>
+      <MainContainer>
+        <FlexLayout>
+          <ContentLayout>
+            <Error404BigTitle>Error 404</Error404BigTitle>
+            <Error404SmallTitle>Something went wrong.</Error404SmallTitle>
+            <Error404ContentTop>
+              We can’t seem to find the page you are looking for.
+            </Error404ContentTop>
+            <Error404HomeButton>
+              <div>
+                <img src={ArrowLeft} alt="ArrowLeft" />
+              </div>
+              <Error404HomeButtonContent>
+                Go back to Home Page
+              </Error404HomeButtonContent>
+            </Error404HomeButton>
+            <Error404ContentBottom>
+              Maybe these categories can help you find what you are looking for
+              :
+            </Error404ContentBottom>
+            <Error404PageLinks>
+              <Link href="/login">Reparatie</Link>
+              <Link href="/over-ons">Over Ons</Link>
+              <Link href="/veel-gestelde-vragen">FAQ</Link>
+              <Link href="/login">Repair shops nearby</Link>
+              <Link href="/login">Other cities</Link>
+              <Link href="/login">Prices</Link>
+            </Error404PageLinks>
+          </ContentLayout>
+          <ImageLayout>
+            <img src={Error404} alt="Error404" />
+          </ImageLayout>
+        </FlexLayout>
+      </MainContainer>
+      <ImageLayoutMobile>
+        <img src={Error404_mobile} alt="Error404_mobile" />
+      </ImageLayoutMobile>
+    </DefaultLayout>
+  );
+};
+
+export default Error404View;
